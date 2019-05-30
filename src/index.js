3eae873323783e1b5fdfc99abeb9313d557e4476
ass233 <1026145889@qq.com>
2019-05-31 00:52:21 +0800

    '打包后的文件'

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 $('#app').html('ass')
 require('./index.css')
-=======
-console.log('hello')
-document.getElementById('app').innerHTML='ass'
->>>>>>> fdd59e1da6c80f368fd2a20a7be83bc14be2d79b
